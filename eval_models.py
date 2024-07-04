aab76b94f9bcd556e851d036ac15189d1e0f7452
Gustavo Araujo Costa <gustavo@arcos.org.br>
2024-07-04 00:43:51 -0300

    add gpt4o functionality

--- eval_models.py
+++ eval_models.py
@@ -1,7 +1,8 @@
 from utils.my_types import Model
 
 models: list[Model] = [
-    Model("llama3"),
+    # Model("llama3"),
+    Model("gpt-4o"),
     # Model("phi3"),
     # Model("mistral"),
 ]
--- eval_prompts.py
+++ eval_prompts.py
@@ -2,37 +2,51 @@ from utils.my_types import Prompt
 
 prompts: list[Prompt] = [
     Prompt(
-        id="menciona_0",
+        id="stj1",
         slots="{variable}",
-        content="""Sua tarefa é analisar e responder se:
-            a. o texto a seguir menciona a necessidade de comprar remédios ou itens de saúde; ou
-            b. o texto menciona um evento imprevisto e excepcional que tenha levado à busca de benefício; ou
-            c. nenhum dos dois.
-            Aqui está o texto:
-            ###
-            {variable}
-            ###
-            Primeiro, analise cuidadosamente o texto em um rascunho. Depois, responda "<<A >> (INDEFERIDO - SAÚDE)", "<<B>> (INDEFERIDO - EVENTO)" ou "<<C>> (DEFERIDO)".""",
+        content="""Responda as seguintes perguntas sobre o texto a seguir. Responda em formato JSONL. Caso a resposta envolva nomes, use apenas os primeiros nomes dos ministros. Caso não haja dados para necesários para responder, responda com "null".
+        
+Perguntas:
+
+1. qual foi o resultado da decisão tomada? responda com "ACEITO", "PARCIAL" ou "NEGADO"
+2. a decisão foi tomada em unanimidade? responda com "SIM" OU "NAO"
+3. houve modificativos? responda "SIM" ou "NAO"
+4. houve conhecer? responda com "SIM", "PARCIAL" ou "NAO"
+5. qual ministro presidiu o julgamento?
+6. houve algum ministro ausente?
+7. quais ministros votaram a favor?
+8. algum ministro foi vencido?
+9. algum ministro lavrará o acórdão?
+10. de quem foi o voto de desempate?
+11. quais ministros fizeram voto-vista?
+
+
+Exemplo de formato da resposta:
+        
+{"resultado": "ACEITO","unanimidade": "SIM","modificativos": "NAO","conhecer": "PARCIAL","presidiu": "Paulo","ausente": null,"aFavor": "Benedito, Sérgio, Regina, Gurgel","vencidos": null,"lavrara": null,"desempate": null,"votoVista": "Regina"}
+
+
+Aqui está o texto:
+###
+{variable}
+###
+""",
     ),
-    # Prompt(
-    #     id="deferimento",
-    #     slots="{variable}",
-    #     content="""Você irá analisar uma solicitação de benefício e determinar se ela  deve ser deferida ou indeferida com base nas regras fornecidas.
-    # Aqui estão as regras para determinar a elegibilidade:
-    # ###
-    # 1. O benefício não pode ser deferido para a compra de remédios, insumos de saúde, órteses ou próteses.
-    # 2. O benefício não pode ser deferido se não houver um fato novo e excepcional relatado no parecer.
-    # ###
-    # Aqui está a solicitação a ser analisada:
-    # ###
-    # {variable}
-    # ###
-    # Primeiro, analise cuidadosamente a solicitação e as regras em um <rascunho>. Considere se a solicitação menciona a necessidade de comprar remédios ou itens de saúde (o que levaria a um indeferimento pela regra 1) e se ela relata um evento imprevisto e excepcional que desorganizou a família (necessário para um deferimento pela regra 2).
-    # Depois de analisar, forneça sua resposta final em uma tag <resposta>. Responda:
-    # "A" se o parecer não contém um evento imprevisto que desorganizou a família
-    # "B" se o benefício não pode ser utilizado para adquirir medicamentos, insumos de saúde, órteses ou
-    # próteses
-    # "C" se o parecer contém um evento inesperado que desorganizou a família
-    # Inclua uma breve justificativa junto com a letra da resposta.""",
-    # ),
 ]
+
+
+# @dataclass
+# class Decisao:
+#     id: str
+#     decisao: str
+#     unanimidade: bool
+#     modificativos: bool
+#     resultado: str  # "aceito" | "parcial" | "negado"
+#     conhecer: Optional[str]  # "sim" | "parcial" | "nao" | NaN
+#     aFavor: Optional[str]  # ministros
+#     vencidos: Optional[str]  # ministros
+#     presidiu: Optional[str]  # ministros
+#     ausente: Optional[str]  # ministros
+#     lavrara: Optional[str]  # ministros
+#     desempate: Optional[str]  # ministros
+#     votoVista: Optional[str]  # ministros
